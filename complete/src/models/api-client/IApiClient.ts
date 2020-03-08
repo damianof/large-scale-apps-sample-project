1a06a3c21cb4cc8798783a14cbf3241c7ae7f659
Damiano <damiano@iMacRetina.local>
2020-03-08 14:52:39 -0400

    cleanup

--- complete/src/models/api-client/IApiClient.ts
+++ complete/src/models/api-client/IApiClient.ts
@@ -1,10 +1,8 @@
 import { IItemsApiClient } from '@/models/api-client/items/IItemsApiClient'
 // GEN-IMPORTS
-import { IPeopleApiClient } from '@/models/api-client/people/IPeopleApiClient'
 
 export interface IApiClient {
 	items: IItemsApiClient
 	// GEN-PROPERTIES
-	people: IPeopleApiClient
 }
 
--- complete/src/models/people/IPerson.ts
+++
@@ -1,4 +0,0 @@
-export interface IPerson {
-	id: number
-	name: string
-}
