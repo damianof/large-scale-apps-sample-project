579288db3034bffe608c6da7fc7c2bf928173b94
Damiano <damiano@iMacRetina.local>
2020-03-26 10:14:25 -0400

    Code comments

--- complete/src/models/store/mutation-types/index.ts
+++ complete/src/models/store/mutation-types/index.ts
@@ -2,8 +2,10 @@ import { IItemsMutationType, ItemsMutationType } from './IItemsMutationType'
 
 export interface IMutationType {
     items: IItemsMutationType
+	// as you add new state modules, add reference to module-specific mutation types here
 }
 
 export const MutationType: IMutationType = {
     items: ItemsMutationType
+	// as you add new state modules, add reference to module-specific mutation types here
 }
