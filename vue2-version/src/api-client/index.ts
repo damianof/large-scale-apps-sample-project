4e825b18ffc4d540370462fda7e91f331309bef5
Damiano <damiano@iMacRetina.local>
2020-08-08 16:58:23 -0400

    Added npm package cross-env

--- vue2-version/src/api-client/index.ts
+++ vue2-version/src/api-client/index.ts
@@ -2,13 +2,6 @@ import { IApiClient } from '@/models/api-client/IApiClient'
 import apiMockClient from '@/api-client/mock'
 import apiLiveClient from '@/api-client/live'
 
-interface process {
-  env: {
-    VUE_APP_TOKEN_KEY: string
-    VUE_APP_API_CLIENT: string
-  }
-}
-
 const env =
   process.env && process.env.VUE_APP_API_CLIENT
     ? process.env.VUE_APP_API_CLIENT
--- vue2-version/src/shims-vue.d.ts
+++ vue2-version/src/shims-vue.d.ts
@@ -2,3 +2,10 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare interface process {
+  env: {
+    VUE_APP_TOKEN_KEY: string
+    VUE_APP_API_CLIENT: string
+  }
+}
